3c533892fa6ceca3218794d5779f32cb7ac3c060
Mamdouh hisham <102166276+MamdouhHisham@users.noreply.github.com>
2023-07-09 17:27:44 +0300

    B. Andryusha and Socks.cpp

--- Prefix sum , Frequency Array #1 /B. Andryusha and Socks.cpp
+++ Prefix sum , Frequency Array #1 /B. Andryusha and Socks.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n; cin >> n;
+    vector<long long> fre(n + 1 , 0);
+    long long mx = INT_MIN , cur = 0;
+    for(int i = 0 ; i < 2 * n ; i++){
+        long long num; cin >> num;
+        cur++;
+        if(fre[num] == 1) cur -= 2;
+        fre[num] = 1;
+        mx = max(mx , cur);
+    }
+    cout << mx << '\n';
+}
 
