84a95b2d612dbd826c88436ee2489315040ed2b6
Mamdouh hisham <102166276+MamdouhHisham@users.noreply.github.com>
2023-07-10 18:16:22 +0300

    B. Tickets.cpp

--- STL #1/B. Tickets.cpp
+++ STL #1/B. Tickets.cpp
@@ -1,17 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    long long n; cin >> n;
-    queue<long long> ids;
+#define ll long long
+#define ld long double
+#define int long long
+#define all(v) v.begin(), v.end()
+const int mod = 1e9 + 7;
+ll oo = 1e18;
+string ys = "YES", no = "NO";
+
+void solve(){
+    ll n; cin >> n;
+    queue<ll> ids;
     while (n--){
-        long long type , id;
+        ll type , id;
         cin >> type >> id;
         if(type == 1){
             ids.push(id);
         }
         else {
-            long long temp = ids.front();
+            ll temp = ids.front();
             if(temp == id)
                 cout << "Yes" << '\n';
             else
@@ -20,4 +29,15 @@ int main() {
             ids.pop();
         }
     }
+}
+
+
+int32_t main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t = 1; //cin >> t;
+    while (t--) { solve();}
 }
