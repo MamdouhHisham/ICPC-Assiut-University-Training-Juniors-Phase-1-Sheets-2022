776fb949c2d8f278404a05ec2b596c32f6ba1c24
Mamdouh hisham <102166276+MamdouhHisham@users.noreply.github.com>
2023-07-10 18:15:47 +0300

    C. Study INTRO!.cpp

--- STL #1/C. Study INTRO!.cpp
+++ STL #1/C. Study INTRO!.cpp
@@ -1,16 +1,25 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-    long long n; cin >> n;
+#define ll long long
+#define ld long double
+#define int long long
+#define all(v) v.begin(), v.end()
+const int mod = 1e9 + 7;
+ll oo = 1e18;
+string ys = "YES", no = "NO";
+
+void solve(){
+    ll n; cin >> n;
     stack<string> tags;
-    long long cnt = 0;
+    ll cnt = 0;
     bool flag = true;
     while (n--){
         string s; cin >> s;
         if(flag && s != "Header"){
             cout << "WA" << '\n';
-            return 0;
+            return;
         }
         flag = false;
         if(s == "Header"){
@@ -18,7 +27,7 @@ int main() {
         }
         if(cnt > 1){
             cout << "WA" << '\n';
-            return 0;
+            return;
         }
         if(tags.empty()){
             tags.push(s);
@@ -36,4 +45,15 @@ int main() {
     } else {
         cout << "WA" << '\n';
     }
+}
+
+
+int32_t main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t = 1; //cin >> t;
+    while (t--) { solve();}
 }
