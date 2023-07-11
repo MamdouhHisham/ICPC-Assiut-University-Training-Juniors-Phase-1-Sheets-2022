9d317ef4097e782781c3fceed55172c45d9fbfe3
Mamdouh hisham <102166276+MamdouhHisham@users.noreply.github.com>
2023-07-11 15:58:41 +0300

    Create C. Sort the array.cpp

---
+++ Standard #1 (Frequency , prefix sum , vector , pair ,struct)/C. Sort the array.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define int long long
+#define all(v) v.begin(), v.end()
+const int mod = 1e9 + 7;
+ll oo = 1e18;
+string ys = "YES", no = "NO";
+
+void solve(){
+    ll n; cin >> n;
+    vector<ll> v(n);
+    for(auto &i : v)
+        cin >> i;
+    sort(all(v));
+    for(auto i : v)
+        cout << i << " ";
+}
+
+
+int32_t main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t = 1; //cin >> t;
+    while (t--) { solve();}
+}
