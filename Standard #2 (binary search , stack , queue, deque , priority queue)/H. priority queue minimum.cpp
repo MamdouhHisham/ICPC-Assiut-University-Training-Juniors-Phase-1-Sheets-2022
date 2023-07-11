a732a4d3ad109e74bf2607a2f4e2c712329f723e
Mamdouh hisham <102166276+MamdouhHisham@users.noreply.github.com>
2023-07-11 16:17:58 +0300

    Create H. priority queue minimum.cpp

---
+++ Standard #2 (binary search , stack , queue, deque , priority queue)/H. priority queue minimum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define int long long
+#define all(v) v.begin(), v.end()
+const int mod = 1e9 + 7;
+ll oo = 1e18;
+string ys = "YES", no = "NO";
+
+void solve() {
+    ll n; cin >> n;
+    priority_queue<ll , vector<ll> , greater<ll>> q;
+    while (n--) {
+        string s; cin >> s;
+        if(s == "push"){
+            ll num; cin >> num;
+            q.push(num);
+        } else if(s == "pop"){
+            q.pop();
+        } else if (s == "top"){
+            cout << q.top() << '\n';
+        }
+    }
+}
+
+int32_t main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    //cout << setprecision(9) << fixed;
+    int t = 1; //cin >> t;
+    while (t--) { solve();}
+}
